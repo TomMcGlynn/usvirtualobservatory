9f8a8a272d6154adb4838fc5bfc21226c20588ce
usvirtualobservatory <usvirtualobservatory@5a1e9bf7-f4d4-f7d4-5b89-e7d39643c4b5>
2014-09-08 17:00:35 +0000

    committing usvao/prototype/vaopy

---
+++ usvao/prototype/vaopy/rc/1.0beta2/python/vaopy/dal/__init__.py
@@ -0,0 +1,5 @@
+#from .imageaccess import sia, SIAService, SIAQuery, SIARecord
+#from .spectralaccess import ssa, SSAService, SSAQuery, SSARecord
+#from .conesearch import conesearch, SCSService, SCSQuery, SCSRecord
+
+
---
+++ usvao/prototype/vaopy/tickets/VAOPD-892/python/vaopy/dal/__init__.py
@@ -0,0 +1,18 @@
+from .sia import search as imagesearch
+from .ssa import search as spectrumsearch
+from .sla import search as linesearch
+from .scs import search as conesearch
+
+from .sia import SIAService, SIAQuery, SIARecord
+from .ssa import SSAService, SSAQuery, SSARecord
+from .sla import SLAService, SLAQuery, SLARecord
+from .scs import SCSService, SCSQuery, SCSRecord
+
+from .query import *
+
+__all__ = [ "imagesearch", "spectrumsearch", "linesearch", "consesearch",
+            "SIAService", "SSAService", "SLAService", "SCSService",
+            "SIAResults", "SSAResults", "SLAResults", "SCSResults",
+            "SIARecord",  "SSARecord",  "SLARecord",  "SCSRecord",
+            "DALResults" ]
+
